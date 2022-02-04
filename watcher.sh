25f78e02b89d3ce0c3e85eefaa5700712166bbfb
Mario Dian <mariodian@gmail.com>
2022-02-04 22:11:26 +0800

    Add option to only receive notification about active dispensers

--- watcher.sh
+++ watcher.sh
@@ -57,31 +57,38 @@ do
 
   # Process data if any available
   if [[ $(echo $JSON | jq -r '.total') > 0 ]]; then
-    LAST_BLOCK=$(echo $JSON | jq '.data[0] | .block_index')
-    SAT_RATE=$(echo $JSON | jq -r '.data[0] | .satoshirate')
-    GIVE_QUANTITY=$(echo $JSON | jq -r '.data[0] | .give_quantity')
-    TX_HASH=$(echo $JSON | jq -r '.data[0] | .tx_hash')
+    GIVE_REMAINING=$(echo $JSON | jq -r '.data[0] | .give_remaining' | cut -f1 -d".")
 
-    # Read existing data about assets
-    touch -a $DATA_FILE
-    IFS="," read -ra A <<< "$(grep $ASSET $DATA_FILE)"
-    CSV_ASSET=${A[0]}
-    CSV_BLOCK=${A[1]}
+    # Only process data if dispenser is active or the NOTIFY_ACTIVE_ONLY is set to false
+    if [[ "$GIVE_REMAINING" -ne "0" || $NOTIFY_ACTIVE_ONLY = "false" ]]; then
+      LAST_BLOCK=$(echo $JSON | jq '.data[0] | .block_index')
+      SAT_RATE=$(echo $JSON | jq -r '.data[0] | .satoshirate')
+      GIVE_QUANTITY=$(echo $JSON | jq -r '.data[0] | .give_quantity')
+      TX_HASH=$(echo $JSON | jq -r '.data[0] | .tx_hash')
 
-    # There's currently no data so we saved the fetched one
-    if [ -z "$CSV_ASSET" ]; then
-      echo "Saving $ASSET data."
-      echo "$ASSET,$LAST_BLOCK" >> $DATA_FILE
-      prepare_notification
-    else
-      # Update with new data
-      if [ $LAST_BLOCK -gt $CSV_BLOCK ]; then
-        echo "Updating $ASSET on block $LAST_BLOCK."
-        awk 'BEGIN{FS=OFS=","} $1=="'$ASSET'"{$2="'$LAST_BLOCK'"} 1' $DATA_FILE > tmp && mv tmp $DATA_FILE
+      # Read existing data about assets
+      touch -a $DATA_FILE
+      IFS="," read -ra A <<< "$(grep $ASSET $DATA_FILE)"
+      CSV_ASSET=${A[0]}
+      CSV_BLOCK=${A[1]}
+
+      # There's currently no data so we saved the fetched one
+      if [ -z "$CSV_ASSET" ]; then
+        echo "Saving $ASSET data."
+        echo "$ASSET,$LAST_BLOCK" >> $DATA_FILE
         prepare_notification
-      else 
-        echo "No new changes available."
+      else
+        # Update with new data
+        if [ $LAST_BLOCK -gt $CSV_BLOCK ]; then
+          echo "Updating $ASSET on block $LAST_BLOCK."
+          awk 'BEGIN{FS=OFS=","} $1=="'$ASSET'"{$2="'$LAST_BLOCK'"} 1' $DATA_FILE > tmp && mv tmp $DATA_FILE
+          prepare_notification
+        else 
+          echo "No new changes available."
+        fi
       fi
+    else
+      echo "No active dispensers available."
     fi
   else
     echo "No dispensers available."
